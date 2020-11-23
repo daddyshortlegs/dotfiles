e1297ce2fd6cb82634c4283b71fdb9fb2fa66cb4
andys <andysmith836@gmail.com>
2020-11-23 21:13:30 +0000

    Setup git a bit nicer

--- setup.sh
+++ setup.sh
@@ -2,4 +2,11 @@ script_path=`dirname $0`
 echo $script_path
 brew bundle --file ${script_path}/Brewfile
 brew bundle --file ${script_path}/devops.Brewfile
-ln -s ${script_path}/.zshrc ~
+
+for config in `ls -a ${script_path}/configs/`
+do
+  if [ ! -e ~/${config} ]; then
+    ln -s ${script_path}/configs/${config} ~
+  fi
+done
+
