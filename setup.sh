7c14c5f8e587f9e9b408f8d0b9f5d4d0bc966f9f
andys <andysmith836@gmail.com>
2020-11-24 22:35:14 +0000

    Enable syntax highlighting by default in vim and make sure pythong and ruby are installed

--- setup.sh
+++ setup.sh
@@ -7,6 +7,12 @@ do
   brew bundle --file ${brew}
 done
 
+rbenv install 2.6.6
+rbenv global 2.6.6
+pyenv install 3.9.0
+pyenv global 3.9.0
+
+echo "Linking dotfiles"
 for config in `ls -a ${script_path}/configs/`
 do
   if [ ! -e ~/${config} ]; then
