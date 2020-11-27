8c68ba25c780dd25321fefa2b2ceafb01e01f938
andys <andysmith836@gmail.com>
2020-11-27 21:40:10 +0000

    Fix iterm2 preferences loading

--- setup.sh
+++ setup.sh
@@ -21,7 +21,7 @@ do
 done
 
 # Specify the preferences directory
-defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "${script_path}/iterm2"
+defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/${script_path}/iterm"
 # Tell iTerm2 to use the custom preferences in the directory
 defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
 
